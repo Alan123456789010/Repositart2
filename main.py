054fa13f6383d803482cdbae757c3c7c8a0d0515
alan <alan.tam@online.island.edu.hk>
2023-04-25 10:25:19 +0800

    commit

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 
 # Press Shift+F10 tpo execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and setti
-print("Hello world!")
+print("Hello world! I like pies")
